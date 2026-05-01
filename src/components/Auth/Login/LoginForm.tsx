df670324fcd597038d506dd7485296ddf632ab26
toufiqulislamtanmoy <tittanmoy@gmail.com>
2026-05-01 10:39:04 +0600

    login from validation done

--- src/components/Auth/Login/LoginForm.tsx
+++ src/components/Auth/Login/LoginForm.tsx
@@ -1,12 +1,14 @@
 "use client";
 import { Field, Form, Formik } from "formik";
+import { useState } from "react";
 import * as Yup from "yup";
 const LoginSchema = Yup.object().shape({
-  email: Yup.string().email("Invalid email").required("Required"),
-  password: Yup.string().required("Required "),
+  email: Yup.string().email("Invalid email").required("Enter your email"),
+  password: Yup.string().required("Enter your password"),
 });
 
 const LoginForm = () => {
+  const [isPasswordVisible, setPasswordVisible] = useState(false);
   return (
     <div>
       <Formik
@@ -37,10 +39,23 @@ const LoginForm = () => {
               <label className="text-sm font-medium text-gray-700 block mb-2">Password</label>
               <Field
                 name="password"
-                type="password"
+                type={isPasswordVisible ? "text" : "password"}
                 placeholder="Enter your password"
                 className="w-full h-12 px-4 rounded-full border border-gray-300 focus:outline-none focus:ring-2 focus:ring-gray-200"
               />
+              <label
+                htmlFor="show_password"
+                className="flex item-center gap-1 lg:ml-2 mt-3 ml-0 text-xs"
+              >
+                <input
+                  type="checkbox"
+                  name="show_password"
+                  id=""
+                  checked={isPasswordVisible}
+                  onChange={() => setPasswordVisible(!isPasswordVisible)}
+                />
+                <span className="">Show Password</span>
+              </label>
               {errors.password && touched.password ? (
                 <p className="text-red-500 text-xs my-1 ml-3">{errors.password}</p>
               ) : null}
